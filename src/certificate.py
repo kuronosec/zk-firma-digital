bf115dba1581d5b2fedb67dc549295437cae5561
kurono <kurono@riseup.net>
2024-09-19 18:22:30 -0600

    Simplify the GUI to generate the JSON credential

---
+++ src/certificate.py
@@ -0,0 +1,66 @@
+import pprint
+import os
+
+from asn1crypto import pem, x509
+from PyKCS11 import *
+
+class Certificate:
+    def __init__(self, pin):
+        """
+            Initialize stuff
+        """
+        self.pin = pin
+
+        # Check what operation system we re running on
+        if os.name == 'nt':
+            self.library_path = 'todo'
+        else:
+            self.library_path = '/usr/lib/x64-athena/libASEP11.so'
+
+    def get_certificates(self):
+        """
+        Try to read the smart card to get the stored public certificates
+        """
+        pkcs11 = PyKCS11Lib()
+        try:
+            pkcs11.load(self.library_path)
+        except PyKCS11Error as error:
+            message = "Hubo un error al cargar la libreria de la smart card"
+            print(message+" "+str(error))
+            return message
+
+        session = None
+
+        try:
+            session = pkcs11.openSession(0, CKF_SERIAL_SESSION | CKF_RW_SESSION)
+            session.login(self.pin)
+        except PyKCS11Error as error:
+            message = """Hubo un error al leer la tarjeta,\
+                         por favor verifique que esta conectada correctamente\
+                         y que ingreso el pin correcto."""
+            print(message+" "+str(error))
+            return message
+
+        result = []
+        certs = session.findObjects([(CKA_CLASS, CKO_CERTIFICATE)])
+
+        for cert in certs:
+            cka_value, cka_id = session.getAttributeValue(cert, [CKA_VALUE, CKA_ID])
+            cert_der = bytes(cka_value)
+            cert = x509.Certificate.load(cert_der)
+            common_name = cert.native["tbs_certificate"]["subject"]["common_name"]
+            if "AUTENTICACION" in common_name:
+                result.append(common_name)
+
+                directory = "../build"
+                if not os.path.exists(directory):
+                    os.makedirs(directory)
+
+                # cert is an instance of x509.Certificate
+                with open("../build/certificado.cert", 'wb') as f:
+                    pprint.pprint(cert.native["tbs_certificate"]["subject"])
+                    der_bytes = cert.dump()
+                    pem_bytes = pem.armor('CERTIFICATE', der_bytes)
+                    f.write(pem_bytes)
+
+        return result
---
+++ src/circom.py
@@ -0,0 +1,83 @@
+import os.path
+
+from zkpy.circuit import Circuit, GROTH, PLONK, FFLONK
+from zkpy.ptau import PTau
+
+class Circom():
+    def __init__(self) -> None:
+        # define variables
+        self.circ_file="../firma-verifier.circom"
+
+        self.output_dir="../build/"
+
+        self.js_dir=self.output_dir+"firma-verifier_js"
+        self.r1cs=self.output_dir+"firma-verifier.r1cs"
+        self.sym_file=self.output_dir+"firma-verifier.sym"
+        self.wasm=self.output_dir+"firma-verifier.wasm"
+        self.witness=self.output_dir+"witness.wtns"
+        self.zkey=self.output_dir+"firma-verifier.zkey"
+        self.ptau_file=self.output_dir+"firma-verifier-final.ptau"
+        self.input_file="../build/input.json"
+        self.zkey_file=self.output_dir+"firma-verifier.zkey"
+        self.output_file=self.output_dir+"vkey.json"
+        self.vkey_file=self.output_dir+"vkey.json"
+        self.public_file=self.output_dir+"public.json"
+        self.proof_file=self.output_dir+"proof.json"
+
+        print("Open circuit")
+        self.circuit = Circuit(circ_file=self.circ_file,
+                        output_dir=self.output_dir,
+                        js_dir=self.js_dir,
+                        r1cs=self.r1cs,
+                        sym_file=self.sym_file,
+                        wasm=self.wasm,
+                        witness=self.witness,
+                        zkey=self.zkey)
+
+        self.ptau = PTau(ptau_file=self.ptau_file,
+                        working_dir="./")
+
+    def compile_circuit(self) -> None:
+        print("Compile circuit")
+        print(self.circuit.compile())
+        print(self.circuit.check_circ_compiled())
+        print("Get info")
+        print(self.circuit.get_info())
+
+    def power_of_tau(self) -> None:
+        print("Power of Tau ceremony")
+        if(not os.path.isfile(self.ptau_file)):
+            print("start()")
+            self.ptau.start(curve='bn128', constraints='23')
+            print("contribute()")
+            self.ptau.contribute()
+            print("beacon()")
+            self.ptau.beacon()
+            print("prep_phase2()")
+            self.ptau.prep_phase2()
+
+    def generate_witness(self) -> None:
+        if(not os.path.isfile(self.witness)):
+            print("circuit.gen_witness")
+            self.circuit.gen_witness(self.input_file)
+
+    def setup(self) -> None:
+        if(not os.path.isfile(self.output_file)):
+            print("setup")
+            self.circuit.setup(GROTH, self.ptau)
+
+    def prove(self) -> None:
+        print("prove")
+        self.circuit.prove(GROTH)
+        print("export_vkey")
+        self.circuit.export_vkey(zkey_file=self.zkey_file,
+                            output_file=self.output_file)
+
+    def verify(self) -> None:
+        if(os.path.isfile(self.vkey_file)
+        and os.path.isfile(self.public_file)
+        and os.path.isfile(self.proof_file)):
+            print("verify")
+            self.circuit.verify(GROTH, vkey_file=self.vkey_file,
+                        public_file=self.public_file,
+                        proof_file=self.proof_file)
