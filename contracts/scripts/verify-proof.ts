a27d10655c673d2b74cb6ec1d7b24efb60fdbd2a
kurono <kurono@riseup.net>
2024-10-17 15:20:18 -0600

    improve on-chain proof verification

--- contracts/scripts/verify-proof.ts
+++ contracts/scripts/verify-proof.ts
@@ -4,19 +4,35 @@
 // Assume proof was generated using production public key.
 
 import '@nomiclabs/hardhat-ethers'
+import { Groth16Proof } from 'snarkjs'
 import { ethers } from 'hardhat'
-import { packGroth16Proof } from '../src'
+
+type BigNumberish = string | bigint
+
+export type PackedGroth16Proof = [
+  BigNumberish,
+  BigNumberish,
+  BigNumberish,
+  BigNumberish,
+  BigNumberish,
+  BigNumberish,
+  BigNumberish,
+  BigNumberish
+]
 
 async function main() {
   // Assumes proof is created in below path (using scripts in packages/circuits)
-  const proof = require('../../circuits/build/proofs/proof.json')
-  const publicInputs = require('../../circuits/build/proofs/public.json')
+  const verifiableCredential = require('../../build/example-credential/credential.json')
 
   const { ZKFirmaDigital } = require(
-    `../deployed-contracts/${process.env.HARDHAT_NETWORK}.json`,
-  ).productionPublicKey
+    `../deployed-contracts/ethereum.json`,
+  ).localPublicKey
 
+  const nullifierSeed = verifiableCredential.proof.signatureValue.public[3]
+  const nullifier = verifiableCredential.proof.signatureValue.public[4]
   const signal = 1 // Signal used when generating proof
+  const revealArray = [1] // For the moment this is assumed always the case that age > 18
+  const proof = verifiableCredential.proof.signatureValue.proof
 
   const ZKFirmaDigitalVerifier = await ethers.getContractAt(
     'ZKFirmaDigital',
@@ -25,16 +41,39 @@ async function main() {
 
   const address = await ZKFirmaDigitalVerifier.getAddress()
   console.log(`ZKFirmaDigital : ${address}`)
+  console.log(`nullifierSeed : ${nullifierSeed}`)
+  console.log(`nullifier : ${nullifier}`)
+  console.log(`proof : ${packGroth16Proof(proof)}`)
 
   console.log(
     await ZKFirmaDigitalVerifier.verifyZKFirmaDigitalProof(
-      publicInputs[0],
-      publicInputs[1],
-      publicInputs[2],
+      nullifierSeed,
+      nullifier,
       signal,
+      revealArray,
       packGroth16Proof(proof),
     ),
   )
+}
+
+/**
+ * Packs a proof into a format compatible with ZKFirmaDigital.sol contract.
+ * @param originalProof The proof generated with SnarkJS.
+ * @returns The proof compatible with Semaphore.
+ */
+function packGroth16Proof(
+  groth16Proof: Groth16Proof
+): PackedGroth16Proof {
+  return [
+    groth16Proof.pi_a[0],
+    groth16Proof.pi_a[1],
+    groth16Proof.pi_b[0][1],
+    groth16Proof.pi_b[0][0],
+    groth16Proof.pi_b[1][1],
+    groth16Proof.pi_b[1][0],
+    groth16Proof.pi_c[0],
+    groth16Proof.pi_c[1],
+  ]
 }
 
 // We recommend this pattern to be able to use async/await everywhere
