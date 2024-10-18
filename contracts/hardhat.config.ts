a0cbf3a08d0703ca033693800b8ac6de423a664b
kurono <kurono@riseup.net>
2024-10-18 12:54:33 -0600

    allow user to cast vote to PoC proposal. But reusing nullifier, to fix

--- contracts/hardhat.config.ts
+++ contracts/hardhat.config.ts
@@ -2,6 +2,8 @@ import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 import "hardhat-dependency-compiler";
 
+const privateKey = process.env.ETHEREUM_ADDRESS_PRIVATE_KEY || '1';
+
 const config: HardhatUserConfig = {
   solidity: "0.8.19",
   paths: {
@@ -9,6 +11,13 @@ const config: HardhatUserConfig = {
     artifacts: "./src",
     cache: "./src",
     tests: "./src"
+  },
+  networks: {
+    localhost: {
+      // This is just a hardhat testing address, do not reuse in productionq
+      url: "http://127.0.0.1:8545",
+      accounts: [privateKey]
+    }
   }
 };
 
