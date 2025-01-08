bfb6c210ecb36d8af318341494be37412c2fc69d
kurono <kurono@riseup.net>
2025-01-08 16:31:18 -0600

    restore main.py

--- src/main.py
+++ src/main.py
@@ -5,12 +5,8 @@ import sys
 import os
 import json
 import datetime
-import utils
 import logging
-
-from os import listdir
-from os.path import isfile, join
-from pathlib import Path
+from urllib.parse import urlparse, parse_qs
 
 # We will use the PyQt6 to provide a grafical interface for the user
 # TODO: test that it works on Windows
@@ -24,18 +20,15 @@ from PyQt6.QtWidgets import ( QApplication,
                               QTabWidget,
                               QLabel,
                               QFileDialog )
-from PyQt6.QtCore import QTimer, Qt, QTranslator
-from PyQt6.QtGui import QCursor
 
 # Import our own libraries
 from certificate import Certificate
 from verification import Verification
 from signature import Signature
-from encryption import Encryption
-from ethereum_utils import EthereumUtils
 from configuration import Configuration
 from circom import Circom
-from pinata import download_from_pinata
+from authentication_window import AuthenticationWindow
+
 
 class MainWindow(QMainWindow):
     def __init__(self):
@@ -49,15 +42,13 @@ class MainWindow(QMainWindow):
         # Create a QLabel
         self.file_label = QLabel()
         # HTML link to the local file
-        self.file_label.setText(
-            self.tr(f'<a href="file:///{self.config.credential_file}">Haga click aquí para ver el archivo de credencial generado</a>')
-        )
+        self.file_label.setText(f'<a href="file:///{self.config.credential_file}">Haga click aquí para ver el archivo de credencial generado</a>')
 
         # Allow the QLabel to open external links
         self.file_label.setOpenExternalLinks(True)
 
-        self.setWindowTitle(self.tr("Zero Knowledge - Firma Digital"))
-        self.setGeometry(600, 400, 1000, 400)
+        self.setWindowTitle("Zero Knowledge - Firma Digital")
+        self.setGeometry(600, 400, 700, 400)
 
         # Create a QTabWidget
         self.tabs = QTabWidget()
@@ -66,16 +57,8 @@ class MainWindow(QMainWindow):
         # Add tabs
         self.verification_tab = self.create_verification_tab()
         self.signing_tab = self.create_signing_tab()
-        self.certificate_request_tab = self.create_certificate_request_tab()
-        self.medical_certificate_tab = self.create_medical_certificate_tab()
-
-        self.tabs.addTab(self.verification_tab, self.tr("Creación de credencial ZK"))
-        self.tabs.addTab(self.signing_tab, self.tr("Firma de credenciales verificables"))
-        self.tabs.addTab(self.certificate_request_tab, self.tr("Solicitar certificado médico"))
-        self.tabs.addTab(self.medical_certificate_tab, self.tr("Ver certificados médicos"))
-
-        self.eth_utils = EthereumUtils()
-        self.eth_utils.load_contracts()
+        self.tabs.addTab(self.verification_tab, "Creación de credencial ZK")
+        self.tabs.addTab(self.signing_tab, "Firma de credenciales verificables")
 
     def create_verification_tab(self):
         # Create the first tab's content
@@ -87,93 +70,47 @@ class MainWindow(QMainWindow):
         # Create the password field
         self.password_field = QLineEdit()
         self.password_field.setEchoMode(QLineEdit.EchoMode.Password)
-        self.password_field.setPlaceholderText(self.tr("Introduzca el PIN de su tarjeta"))
+        self.password_field.setPlaceholderText("Introduzca el PIN de su tarjeta")
         self.verification_layout.addWidget(self.password_field)
 
         # Create the "Obtener certificados Firma Digital" button
-        self.generate_credential_button = QPushButton(self.tr("Generar credencial JSON"))
-        self.generate_credential_button.clicked.connect(
-            self.on_submit_generate_credential
-        )
-        self.generate_credential_button.setStyleSheet(self.tr("background-color : green"))
+        self.generate_credential_button = QPushButton("Generar credencial JSON")
+        self.generate_credential_button.clicked.connect(self.on_submit_generate_credential)
+        self.generate_credential_button.setStyleSheet("background-color : green")
         self.verification_layout.addWidget(self.generate_credential_button)
 
         # Set the layout for the central widget
         verification_tab.setLayout(self.verification_layout)
-        self.verification_layout.setAlignment(Qt.AlignmentFlag.AlignVCenter)
-
         return verification_tab
 
     def create_signing_tab(self):
         # Create the signature tab's content
         self.signature_tab = QWidget()
         self.signature_layout = QVBoxLayout()
-        self.signature_layout.addWidget(QLabel(self.tr("Firmar archivo JSON")))
+        self.signature_layout.addWidget(QLabel("Firmar archivo JSON"))
 
         # Create the password field
         self.password_field_sign = QLineEdit()
         self.password_field_sign.setEchoMode(QLineEdit.EchoMode.Password)
-        self.password_field_sign.setPlaceholderText(self.tr("Introduzca el PIN de su tarjeta"))
+        self.password_field_sign.setPlaceholderText("Introduzca el PIN de su tarjeta")
         self.signature_layout.addWidget(self.password_field_sign)
 
         # Create a button to open the file dialog
-        button_browser = QPushButton(self.tr("Escoger archivo a firmar"))
+        button_browser = QPushButton("Escoger archivo a firmar")
         button_browser.clicked.connect(self.browse_files)
         self.signature_layout.addWidget(button_browser)
 
         # Label to display the selected file
-        self.browser_label = QLabel(self.tr("Selected file: None"))
+        self.browser_label = QLabel("Selected file: None")
         self.signature_layout.addWidget(self.browser_label)
-        self.signature_layout.setAlignment(Qt.AlignmentFlag.AlignVCenter)
         self.signature_tab.setLayout(self.signature_layout)
 
         # Create a button to sign the file
-        button_sign = QPushButton(self.tr("Firmar archivo"))
+        button_sign = QPushButton("Firmar archivo")
         button_sign.clicked.connect(self.sign_files)
         self.signature_layout.addWidget(button_sign)
 
         return self.signature_tab
-
-    def create_certificate_request_tab(self):
-        # Create the encryption tab's content
-        self.certificate_request_tab = QWidget()
-        self.certificate_request_layout = QVBoxLayout()
-        self.certificate_request_layout.addWidget(QLabel(self.tr("Solicite un certificado médico")))
-
-        # Create the user id field
-        self.id_number_field = QLineEdit()
-        self.id_number_field.setPlaceholderText(self.tr("Introduzca su número de cédula"))
-        self.certificate_request_layout.addWidget(self.id_number_field)
-        self.certificate_request_tab.setLayout(self.certificate_request_layout)
-        self.certificate_request_layout.setAlignment(Qt.AlignmentFlag.AlignVCenter)
-
-        # Create a button to sign the file
-        self.create_request_button = QPushButton(self.tr("Enviar solicitud"))
-        self.create_request_button.setStyleSheet("background-color : green")
-        self.create_request_button.clicked.connect(self.send_medical_request)
-        self.certificate_request_layout.addWidget(self.create_request_button)
-
-        return self.certificate_request_tab
-
-    def create_medical_certificate_tab(self):
-        # Create the medical certificate tab's content
-        self.medical_certificate_tab = QWidget()
-        self.medical_certificate_layout = QVBoxLayout()
-        self.medical_certificate_layout.addWidget(QLabel(self.tr("Descarge su certificados médico")))
-
-        self.message_label = QLabel(self.tr("Buscando certificados..."))
-        self.message_label.setStyleSheet("background-color : green")
-        self.message_label.setAlignment(Qt.AlignmentFlag.AlignCenter)
-        self.medical_certificate_layout.setAlignment(Qt.AlignmentFlag.AlignCenter)
-        self.medical_certificate_layout.addWidget(self.message_label)
-        self.medical_certificate_tab.setLayout(self.medical_certificate_layout)
-
-        # Timer to check for resource availability every 60 seconds
-        self.check_timer = QTimer(self)
-        self.check_timer.timeout.connect(self.check_resource)
-        self.check_timer.start(60000)  # Check every 60 seconds
-
-        return self.medical_certificate_tab
 
     def on_submit_generate_credential(self):
         self.generate_credential_button.setEnabled(False)
@@ -182,7 +119,7 @@ class MainWindow(QMainWindow):
         password = self.password_field.text()
         certificate = Certificate(password)
         (valid, info) = certificate.get_certificates()
-        QMessageBox.information(self, self.tr("Certificados"), f"{info}")
+        QMessageBox.information(self, "Certificados", f"{info}")
         if not valid:
             self.generate_credential_button.setEnabled(True)
             self.generate_credential_button.setStyleSheet("background-color : green")
@@ -190,34 +127,30 @@ class MainWindow(QMainWindow):
         # If the certificates were stored in disk then provide the option
         # to verify them
         if not os.path.exists(self.config.certificate_path):
-            QMessageBox.information(self, self.tr("Certificado"),
-                                    self.tr("No se pudo obtener el certificado"))
+            QMessageBox.information(self, "Certificado", "No se pudo obtener el certificado")
             self.generate_credential_button.setEnabled(True)
             self.generate_credential_button.setStyleSheet("background-color : green")
             return
         # Verify the stored certificates using the Goverment chain of trust
         password = self.password_field.text()
-        verification = Verification(password)
+        ethereum_address = os.getenv(
+            "ETHEREUM_ADDRESS",
+            '0x' + bytes("user", 'utf-8').hex()
+        )
+        verification = Verification(password, signal_hash=ethereum_address)
 
         (valid, info) = verification.verify_certificate(self.config.certificate_path)
         if not valid:
-            QMessageBox.information(
-                self, 
-                self.tr("Validación"),
-                self.tr(f"{info}\n\n Firma de certificado inválida!!!"))
+            QMessageBox.information(self, "Validación", f"{info}\n\n Firma de certificado inválida!!!")
         else:
-            QMessageBox.information(
-                self,
-                self.tr("Validación"),
-                self.tr(f"{info}\n\n Firma de certificado válida!!!")
-            )
+            QMessageBox.information(self, "Validación", f"{info}\n\n Firma de certificado válida!!!")
             try:
                 circom = Circom()
                 circom.generate_witness()
                 circom.prove()
                 circom.verify()
             except Exception as error:
-                message = self.tr("Hubo un error al crear la credencial verificable")
+                message ="Hubo un error al crear la credencial verificable"
                 QMessageBox.information(self, "Circom", message)
                 logging.error(message+" "+str(error), exc_info=True)
                 self.generate_credential_button.setEnabled(True)
@@ -249,10 +182,8 @@ class MainWindow(QMainWindow):
 
             self.verification_layout.addWidget(self.file_label)
 
-            QMessageBox.information(
-                self,
-                self.tr("Creación de credencial válida"),
-                self.tr("Encontrar credencial verificable en el enlace."))
+            QMessageBox.information(self, "Creación de credencial válida",
+                                    "Encontrar credencial verificable en el enlace.")
         self.generate_credential_button.setEnabled(True)
         self.generate_credential_button.setStyleSheet("background-color : green")
     
@@ -300,11 +231,13 @@ class MainWindow(QMainWindow):
                                                    "JSON Files (*.json)")
 
         if file_name:
-            self.browser_label.setText(self.tr(f"Selected file: {file_name}"))
+            self.browser_label.setText(f"Selected file: {file_name}")
             self.file_to_sign = file_name
         else:
-            self.browser_label.setText(self.tr("No file selected"))
+            self.browser_label.setText("No file selected")
 
+    # This code is not being used at the moment
+    # Do we really need it?
     def sign_files(self):
         # Sign selected file
         password = self.password_field_sign.text()
@@ -315,116 +248,42 @@ class MainWindow(QMainWindow):
             file_only_path = os.path.dirname(self.file_to_sign)
             signed_name = file_only_path+"/"+"signed-"+file_name
             info = signature.sign_file(self.file_to_sign)
-            QMessageBox.information(
-                self,
-                self.tr("Firma de archivo JSON"),
-                f"{info}\n\nArchivo JSON firmado:\n\n {signed_name}"
-            )
-            self.browser_label.setText(self.tr(f"Archivo JSON firmado: {signed_name}"))
+            QMessageBox.information(self, "Firma de archivo JSON",
+                                    f"{info}\n\nArchivo JSON firmado:\n\n {signed_name}")
+            self.browser_label.setText(f"Archivo JSON firmado: {signed_name}")
         else:
-            self.browser_label.setText(
-                self.tr("No file selected")
-            )
-
-    def send_medical_request(self):
-        self.create_request_button.setEnabled(False)
-        self.create_request_button.setStyleSheet("background-color : gray")
-        # Sign certificate request number
-        id_number = self.id_number_field.text()
-
-        # Check number not empty
-        if id_number:
-            # Inititialize encryption object and load public key
-            encryption = Encryption("./CA-certificates/public_testing_key.pem")
-            public_key = encryption.load_public_key()
-
-            # Encrypt request id with govement's public key
-            encrypted_id = encryption.encrypt(id_number, public_key)
-
-            # Create on-chain (Polygon amoy) standard verifable credential
-            self.eth_utils.create_verifiable_credential(
-                self.config.credential_file
-            )
-
-            # Check for existing credentials
-            credentials = self.eth_utils.get_credentials()
-
-            # If credentials available, create medical certificate request
-            self.eth_utils.create_medical_credential_request(
-                encrypted_id
-            )
-            QMessageBox.information(self, self.tr("Petición de certificado"),
-                                    self.tr(f"Petición de certificado enviada"))
-        self.create_request_button.setEnabled(True)
-        self.create_request_button.setStyleSheet("background-color : green")
-
-    def check_resource(self):
-        # Check if resource is available
-        medical_certificate = self.eth_utils.get_medical_certificate_document()
-        if medical_certificate != None:
-            encryption = Encryption(
-                "./CA-certificates/public_testing_key.pem",
-                "./CA-certificates/private_testing_key.pem")
-            private_key = encryption.load_private_key()
-
-            # Take ipfs has encryption data and decrypt it
-            ipfs_hash = encryption.decrypt(
-                medical_certificate[1],
-                private_key)
-
-            # Convert the bytes to a string
-            ipfs_hash_str = ipfs_hash.decode("utf-8") if isinstance(
-                ipfs_hash,
-                bytes
-                ) else ipfs_hash
-
-            download_from_pinata(
-                ipfs_hash_str,
-                os.path.join(self.config.user_path,
-                             Path('documents/encrypted_output_pinata.pdf'))
-            )
-
-            aes_key = encryption.decrypt(
-                medical_certificate[2],
-                private_key)
-            encryption.decrypt_pdf_content(
-                os.path.join(self.config.user_path,
-                             Path('documents/encrypted_output_pinata.pdf')),
-                os.path.join(self.config.user_path,
-                             Path('documents/medical-certificate.pdf')),
-                aes_key)
-
-            self.show_link(os.path.join(self.config.user_path,
-                             Path('documents/medical-certificate.pdf')))
-            self.eth_utils.revoke_verifiable_credential(0)
-
-            self.check_timer.stop()  # Stop checking once the resource is found
-
-    def show_link(self, medical_certificate_file):
-        self.message_label.setStyleSheet("background-color : white")
-        self.message_label.setText(
-            self.tr(f'<a href="file:///{medical_certificate_file}">Haga click aquí para ver el archivo de certificado médico</a>'))
-        self.message_label.setOpenExternalLinks(True)
-        self.message_label.setTextInteractionFlags(Qt.TextInteractionFlag.TextBrowserInteraction)
-        self.message_label.setCursor(QCursor(Qt.CursorShape.PointingHandCursor))
-
-def load_language(language_code):
-    translator = QTranslator()
-    if translator.load(f"translations_{language_code}.qm"):
-        print(f"Loaded language: translations_{language_code}.qm")
-        return translator
-    else:
-        print(f"Could not load language: translations_{language_code}.qm")
-        return None
+            self.browser_label.setText("No file selected")
 
 # Main entry point for our app
 if __name__ == "__main__":
-    app = QApplication(sys.argv)
-    language_code = "en"
-    translator = load_language(language_code)
-    app.installTranslator(translator)
 
-    window = MainWindow()
-    window.show()
+    logging.info("Starting ZK-Firma-Digital")
 
-    sys.exit(app.exec())
+    if len(sys.argv) > 1:
+        uri = sys.argv[1]
+
+        # Parse the URI
+        parsed_uri = urlparse(uri)
+        action = parsed_uri.path.strip("/")
+        params = parse_qs(parsed_uri.query)
+
+        # Extract the JWT token from the URI
+        token = params.get("token", [None])[0]
+
+        if parsed_uri.netloc == "authentication" and token:
+            app = QApplication(sys.argv)
+
+            window = AuthenticationWindow(token)
+            window.show()
+
+            sys.exit(app.exec())
+        else:
+            logging.error(
+                "Invalid KYC request or missing parameters.",
+                exc_info=True
+            )
+    else:
+        app = QApplication(sys.argv)
+        window = MainWindow()
+        window.show()
+        sys.exit(app.exec())
