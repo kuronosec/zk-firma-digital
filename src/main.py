db04555bd494006c90261022f5fb675d44567f72
kurono <kurono@riseup.net>
2025-02-23 10:16:36 -0500

    set spanish as the default language

--- src/main.py
+++ src/main.py
@@ -278,7 +278,7 @@ def load_language(language_code):
 if __name__ == "__main__":
 
     logging.info("Starting ZK-Firma-Digital")
-    language_code = "en"
+    language_code = "es"
     translator = load_language(language_code)
 
     if len(sys.argv) > 1:
